bc6c27d8fd02b166894b7bbab5857bcdc5bd478a
Eisha CHEN-YEN-SU <chenyensu0@gmail.com>
2018-05-26 12:35:46 +0200

    [WIP]

--- fractal-gtk/src/widgets/room.rs
+++ fractal-gtk/src/widgets/room.rs
@@ -20,6 +20,10 @@ use std::sync::mpsc::TryRecvError;
 
 use appop::AppOp;
 
+use widgets::image::{Image, Thumb, Circle};
+
+const AVATAR_SIZE: i32 = 60;
+
 // Room Search item
 pub struct RoomBox<'a> {
     room: &'a Room,
@@ -37,37 +41,47 @@ impl<'a> RoomBox<'a> {
     pub fn widget(&self) -> gtk::Box {
         let r = self.room;
 
-        let h = gtk::Box::new(gtk::Orientation::Vertical, 0);
-        let w = gtk::Box::new(gtk::Orientation::Horizontal, 5);
+        let list_row_box = gtk::Box::new(gtk::Orientation::Vertical, 0);
+
+        let widget_box = gtk::Box::new(gtk::Orientation::Horizontal, 12);
+        let details_box = gtk::Box::new(gtk::Orientation::Vertical, 6);
+        let membership_box = gtk::Box::new(gtk::Orientation::Vertical, 6);
+
+        // let h = gtk::Box::new(gtk::Orientation::Vertical, 0);
+        // let w = gtk::Box::new(gtk::Orientation::Horizontal, 5);
 
         let mname = match r.name {
             ref n if n.is_none() || n.clone().unwrap().is_empty() => r.alias.clone(),
             ref n => n.clone(),
         };
 
-        let avatar = gtk::Image::new_from_icon_name("image-missing", 5);
-        let a = avatar.clone();
-        let id = r.id.clone();
-        let name = mname.clone();
-        let (tx, rx): (Sender<String>, Receiver<String>) = channel();
-        self.op.backend.send(BKCommand::GetThumbAsync(r.avatar.clone().unwrap_or_default().clone(), tx)).unwrap();
-        gtk::timeout_add(50, move || match rx.try_recv() {
-            Err(TryRecvError::Empty) => gtk::Continue(true),
-            Err(TryRecvError::Disconnected) => gtk::Continue(false),
-            Ok(fname) => {
-                let mut f = fname.clone();
-                if f.is_empty() {
-                    f = api::util::draw_identicon(&id, name.clone().unwrap_or_default(), api::util::AvatarMode::Circle).unwrap();
-                }
-                if let Ok(pixbuf) = Pixbuf::new_from_file_at_scale(&f, 32, 32, false) {
-                    a.set_from_pixbuf(&pixbuf);
-                }
-                gtk::Continue(false)
-            }
-        });
-        w.pack_start(&avatar, false, false, 0);
+        // let avatar = gtk::Image::new_from_icon_name("image-missing", 5);
+        // let a = avatar.clone();
+        // let id = r.id.clone();
+        // let name = mname.clone();
+        // let (tx, rx): (Sender<String>, Receiver<String>) = channel();
+        // self.op.backend.send(BKCommand::GetThumbAsync(r.avatar.clone().unwrap_or_default().clone(), tx)).unwrap();
+        // gtk::timeout_add(50, move || match rx.try_recv() {
+        //     Err(TryRecvError::Empty) => gtk::Continue(true),
+        //     Err(TryRecvError::Disconnected) => gtk::Continue(false),
+        //     Ok(fname) => {
+        //         let mut f = fname.clone();
+        //         if f.is_empty() {
+        //             f = api::util::draw_identicon(&id, name.clone().unwrap_or_default(), api::util::AvatarMode::Circle).unwrap();
+        //         }
+        //         if let Ok(pixbuf) = Pixbuf::new_from_file_at_scale(&f, 32, 32, false) {
+        //             a.set_from_pixbuf(&pixbuf);
+        //         }
+        //         gtk::Continue(false)
+        //     }
+        // });
+        // w.pack_start(&avatar, false, false, 0);
 
-        let b = gtk::Box::new(gtk::Orientation::Vertical, 0);
+        let avatar = Image::new(&self.op.backend, &r.avatar.clone().unwrap_or_default(), (AVATAR_SIZE, AVATAR_SIZE), Thumb(true), Circle(true));
+
+        widget_box.pack_start(&avatar.widget, true, true, 0);
+
+        // let b = gtk::Box::new(gtk::Orientation::Vertical, 0);
 
         let msg = gtk::Label::new("");
         msg.set_line_wrap(true);
@@ -102,19 +116,42 @@ impl<'a> RoomBox<'a> {
         let buttons = gtk::Box::new(gtk::Orientation::Horizontal, 0);
         buttons.pack_start(&joinbtn, false, false, 0);
 
-        b.add(&msg);
-        b.add(&topic);
-        b.add(&idw);
-        b.add(&buttons);
+        // b.add(&msg);
+        // b.add(&topic);
+        // b.add(&idw);
+        // b.add(&buttons);
 
-        w.pack_start(&b, true, true, 0);
+        details_box.add(&msg);
+        details_box.add(&topic);
+        details_box.add(&idw);
 
-        let members = gtk::Label::new(&format!("{}", r.n_members)[..]);
-        w.pack_start(&members, false, false, 5);
+        // w.pack_start(&b, true, true, 0);
 
-        h.add(&w);
-        h.add(&gtk::Separator::new(gtk::Orientation::Horizontal));
-        h.show_all();
-        h
+        widget_box.pack_start(&details_box, true, true, 0);
+
+        let member_count_box = gtk::Box::new(gtk::Orientation::Horizontal, 6);
+
+        let members_icon = gtk::Image::new_from_icon_name("system-users-symbolic", gtk::IconSize::Menu.into());
+
+        let member_count = gtk::Label::new(&format!("{}", r.n_members)[..]);
+        // w.pack_start(&members, false, false, 5);
+
+        member_count_box.add(&members_icon);
+        member_count_box.add(&member_count);
+
+        membership_box.add(&member_count_box);
+        membership_box.add(&buttons);
+
+        widget_box.pack_start(&membership_box, false, false, 12);
+
+        list_row_box.add(&widget_box);
+        list_row_box.add(&gtk::Separator::new(gtk::Orientation::Horizontal));
+        list_row_box.show_all();
+        list_row_box
+
+        // h.add(&w);
+        // h.add(&gtk::Separator::new(gtk::Orientation::Horizontal));
+        // h.show_all();
+        // h
     }
 }
