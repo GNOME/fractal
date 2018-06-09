8cb9c0d0f72cc3d42d79bce30cdf48a9cbafe884
Eisha CHEN-YEN-SU <chenyensu0@gmail.com>
2018-06-09 17:10:22 +0200

    WIP: Implement zoom

--- fractal-gtk/src/appop/media_viewer.rs
+++ fractal-gtk/src/appop/media_viewer.rs
@@ -2,6 +2,7 @@ extern crate gtk;
 
 use self::gtk::prelude::*;
 
+use uibuilder;
 use appop::AppOp;
 use appop::AppState;
 
@@ -9,13 +10,16 @@ use widgets::image;
 
 use types::Room;
 
+#[derive(Clone)]
 pub struct MediaViewer {
     media_urls: Vec<String>,
     current_url_index: usize,
+
+    image: image::Image,
 }
 
 impl MediaViewer {
-    pub fn from_room(room: &Room, current_media_url: &str) -> MediaViewer {
+    pub fn new(room: &Room, current_media_url: &str, image: image::Image) -> MediaViewer {
         let img_msgs = room.messages.iter().filter(|msg| msg.mtype == "m.image");
         let media_urls: Vec<String> = img_msgs.map(|msg| msg.url.clone().unwrap_or_default()).collect();
 
@@ -24,7 +28,12 @@ impl MediaViewer {
         MediaViewer {
             media_urls,
             current_url_index,
+            image,
         }
+    }
+
+    pub fn set_zoom_level(&self) {
+        unimplemented!()
     }
 }
 
@@ -32,7 +41,6 @@ impl AppOp {
     pub fn display_media_viewer(&mut self, url: String, room_id: String) {
         let rooms = self.rooms.clone();
         let r = rooms.get(&room_id).unwrap();
-        self.media_viewer = Some(MediaViewer::from_room(r, &url));
 
         self.set_state(AppState::MediaViewer);
 
@@ -50,6 +58,18 @@ impl AppOp {
 
         media_viewport.add(&image.widget);
         media_viewport.show_all();
+
+        let ui = self.ui.clone();
+        let zoom_level = image.zoom_level.clone();
+        image.widget.connect_draw(move |_, _| {
+            if let Some(zlvl) = *zoom_level.lock().unwrap() {
+                update_zoom_entry(&ui, zlvl);
+            }
+
+            Inhibit(false)
+        });
+
+        self.media_viewer = Some(MediaViewer::new(r, &url, image));
 
         self.set_nav_btn_sensitivity();
     }
@@ -94,6 +114,18 @@ impl AppOp {
 
             image.widget.show();
             media_viewport.add(&image.widget);
+
+            let ui = self.ui.clone();
+            let zoom_level = image.zoom_level.clone();
+            image.widget.connect_draw(move |_, _| {
+                if let Some(zlvl) = *zoom_level.lock().unwrap() {
+                    update_zoom_entry(&ui, zlvl);
+                }
+
+                Inhibit(false)
+            });
+
+            mv.image = image;
         }
 
         self.set_nav_btn_sensitivity();
@@ -126,6 +158,18 @@ impl AppOp {
 
             image.widget.show();
             media_viewport.add(&image.widget);
+
+            let ui = self.ui.clone();
+            let zoom_level = image.zoom_level.clone();
+            image.widget.connect_draw(move |_, _| {
+                if let Some(zlvl) = *zoom_level.lock().unwrap() {
+                    update_zoom_entry(&ui, zlvl);
+                }
+
+                Inhibit(false)
+            });
+
+            mv.image = image;
         }
 
         self.set_nav_btn_sensitivity();
@@ -154,4 +198,11 @@ impl AppOp {
             }
         }
     }
+}
+
+fn update_zoom_entry(ui: &uibuilder::UI, zoom_level: f64) {
+    let zoom_entry = ui.builder
+        .get_object::<gtk::EntryBuffer>("zoom_level")
+        .expect("Cant find zoom_level in ui file.");
+    zoom_entry.set_text(&zoom_level.to_string());
 }
