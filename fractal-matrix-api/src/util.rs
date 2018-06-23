cde745b88762faeebd52fdfb68e4c6d75c32e81b
Jordan Petridis <jordanpetridis@protonmail.com>
2018-06-23 18:36:58 +0300

    Use rayon::spawn() instead of thread::spawn() wherever possible.
    Instead of spawning extra threads use rayon::spawn() to queue actions
    into the already existing threadpool.

--- fractal-matrix-api/src/util.rs
+++ fractal-matrix-api/src/util.rs
@@ -125,7 +125,9 @@ macro_rules! post {
 #[macro_export]
 macro_rules! query {
     ($method: expr, $url: expr, $attrs: expr, $okcb: expr, $errcb: expr, $timeout: expr) => {
-        thread::spawn(move || {
+        use rayon;
+
+        rayon::spawn(move || {
             let js = json_q($method, $url, $attrs, $timeout);
 
             match js {
