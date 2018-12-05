63ed67ed0bdbd524a10ffcc4210bc957132f1648
Julian Sparber <julian@sparber.net>
2018-12-05 20:10:12 +0100

    media_viewer: use Gactions for the menu and add more entries

--- fractal-gtk/src/actions/mod.rs
+++ fractal-gtk/src/actions/mod.rs
@@ -1,5 +1,7 @@
 pub mod global;
 pub mod room_history;
+pub mod media_viewer;
 
 pub use self::global as Global;
 pub use self::room_history as RoomHistory;
+pub use self::media_viewer as MediaViewer;
