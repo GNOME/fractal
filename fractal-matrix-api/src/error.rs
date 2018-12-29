8f768145bdfebcfd5e250fac3a7af44de497260b
Alejandro Domínguez <adomu@net-c.com>
2018-12-29 22:10:18 +0100

    Backend: Cairo is unused, don't depend on it

--- fractal-matrix-api/src/error.rs
+++ fractal-matrix-api/src/error.rs
@@ -1,4 +1,3 @@
-use cairo;
 use glib;
 use regex;
 use reqwest;
@@ -29,8 +28,6 @@ impl From<reqwest::Error> for Error {
 derror!(url::ParseError, Error::BackendError);
 derror!(io::Error, Error::BackendError);
 derror!(regex::Error, Error::BackendError);
-derror!(cairo::Status, Error::BackendError);
-derror!(cairo::IoError, Error::BackendError);
 derror!(glib::Error, Error::BackendError);
 derror!(SystemTimeError, Error::BackendError);
 
