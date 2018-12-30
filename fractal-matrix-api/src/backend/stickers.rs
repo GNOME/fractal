b0815a2383d586d31ff860a81ddd3fa3383d0042
Alejandro Domínguez <adomu@net-c.com>
2018-12-30 01:06:44 +0100

    Backend: Use Url type in BackendData.scalar_url

--- fractal-matrix-api/src/backend/stickers.rs
+++ fractal-matrix-api/src/backend/stickers.rs
@@ -176,13 +176,13 @@ fn url(
 }
 
 fn get_scalar_token(data: &Arc<Mutex<BackendData>>) -> Result<String, Error> {
-    let s = data.lock().unwrap().scalar_url.clone();
+    let base = data.lock().unwrap().scalar_url.clone();
     let uid = data.lock().unwrap().user_id.clone();
 
     let url = url(data, &format!("user/{}/openid/request_token", uid), &[])?;
     let js = json_q("post", &url, &json!({}), globals::TIMEOUT)?;
 
-    let vurl = Url::parse(&format!("{}/api/register", s))?;
+    let vurl = base.join("/api/register")?;
     let js = json_q("post", &vurl, &js, globals::TIMEOUT)?;
 
     match js["scalar_token"].as_str() {
@@ -199,8 +199,7 @@ fn vurl(
     path: &str,
     params: &[(&str, String)],
 ) -> Result<Url, Error> {
-    let s = data.lock().unwrap().scalar_url.clone();
-    let base = Url::parse(&s)?;
+    let base = data.lock().unwrap().scalar_url.clone();
     let token = data.lock().unwrap().scalar_token.clone();
     let tk = match token {
         None => get_scalar_token(&data)?,
