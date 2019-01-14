72453a9af0d5a4c7ee40ad0a93342a2f5d92686d
Alejandro Domínguez <adomu@net-c.com>
2019-01-14 13:18:21 +0100

    Backend: implement PublicRoomsRequest

--- fractal-matrix-api/src/model/room.rs
+++ fractal-matrix-api/src/model/room.rs
@@ -121,6 +121,35 @@ impl PartialEq for Room {
 
 pub type RoomList = HashMap<String, Room>;
 
+#[derive(Clone, Debug, Serialize)]
+pub struct PublicRoomsRequest {
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub limit: Option<i32>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub since: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub filter: Option<String>,
+    #[serde(flatten)]
+    pub third_party_networks: ThirdPartyNetworks,
+}
+
+#[derive(Clone, Debug, Serialize)]
+#[serde(tag = "include_all_networks", content = "third_party_instance_id")]
+pub enum ThirdPartyNetworks {
+    #[serde(rename = "false")]
+    None,
+    #[serde(rename = "false")]
+    Only(String),
+    #[serde(rename = "true")]
+    All,
+}
+
+impl Default for ThirdPartyNetworks {
+    fn default() -> Self {
+        ThirdPartyNetworks::None
+    }
+}
+
 #[derive(Clone, Debug, Deserialize)]
 pub struct PublicRooms {
     pub chunk: Vec<PublicRoomsChunk>,
