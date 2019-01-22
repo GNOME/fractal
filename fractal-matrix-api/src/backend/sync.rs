0ce2ad74c06021306603bf60a6017864f6e8fba9
Alejandro Domínguez <adomu@net-c.com>
2019-01-22 23:06:15 +0100

    fix

--- fractal-matrix-api/src/backend/sync.rs
+++ fractal-matrix-api/src/backend/sync.rs
@@ -12,7 +12,7 @@ use crate::util::parse_sync_events;
 use log::error;
 use serde_json::json;
 use serde_json::Value as JsonValue;
-use std::{iter, thread, time};
+use std::{thread, time};
 
 pub fn sync(bk: &Backend, new_since: Option<String>, initial: bool) -> Result<(), Error> {
     let tk = bk.data.lock().unwrap().access_token.clone();
@@ -23,8 +23,14 @@ pub fn sync(bk: &Backend, new_since: Option<String>, initial: bool) -> Result<()
     let since = bk.data.lock().unwrap().since.clone().or(new_since);
     let userid = bk.data.lock().unwrap().user_id.clone();
 
-    let (filter, timeout) = if !initial {
-        (None, time::Duration::from_secs(30))
+    let mut params = vec![("full_state", String::from("false"))];
+
+    if let Some(since) = since.clone() {
+        params.push(("since", since));
+    }
+
+    let timeout = if !initial {
+        time::Duration::from_secs(30)
     } else {
         let filter = format!(r#"{{
             "room": {{
@@ -42,15 +48,12 @@ pub fn sync(bk: &Backend, new_since: Option<String>, initial: bool) -> Result<()
             "event_format": "client",
             "event_fields": ["type", "content", "sender", "origin_server_ts", "event_id", "unsigned"]
         }}"#, globals::PAGE_LIMIT);
+        params.push(("filter", filter));
 
-        (Some(filter), Default::default())
+        Default::default()
     };
 
-    let params = iter::once(("full_state", false.to_string()))
-        .chain(since.clone().map(|since| ("since", since)))
-        .chain(filter.map(|filter| ("filter", filter)))
-        .chain(iter::once(("timeout", timeout.as_secs().to_string())))
-        .collect();
+    params.push(("timeout", timeout.as_secs().to_string()));
 
     let baseu = bk.get_base_url();
     let url = bk.url("sync", params)?;
@@ -152,8 +155,7 @@ pub fn sync(bk: &Backend, new_since: Option<String>, initial: bool) -> Result<()
         |err| {
             // we wait if there's an error to avoid 100% CPU
             error!("Sync Error, waiting 10 seconds to respond for the next sync");
-            let wait = time::Duration::from_secs(10);
-            thread::sleep(wait);
+            thread::sleep(time::Duration::from_secs(10));
 
             tx.send(BKResponse::SyncError(err)).unwrap();
         },
