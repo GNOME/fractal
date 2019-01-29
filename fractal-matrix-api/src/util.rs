6b97f93c1dc6f3ecbea9cb76fb16eb6d550a966b
mairandomness <mairakodama@gmail.com>
2019-01-28 19:01:22 -0500

    try to debug the code

--- fractal-matrix-api/src/util.rs
+++ fractal-matrix-api/src/util.rs
@@ -240,10 +240,14 @@ pub fn put_media(url: &str, file: Vec<u8>) -> Result<JsonValue, Error> {
     let conn = client.post(url).body(file).header(CONTENT_TYPE, mime);
 
     let mut res = conn.send()?;
+    println!("this is the res {:#?}", res);
 
     match res.json() {
         Ok(js) => Ok(js),
-        Err(_) => Err(Error::BackendError),
+        Err(e) => {
+            println!("error from put_media {:?}", e);
+            Err(Error::BackendError)
+        }
     }
 }
 
