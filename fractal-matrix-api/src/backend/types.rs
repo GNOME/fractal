b6e079a2e8e22da4501834712a8c767577a93476
Rasmus Rendal <rasmus@rend.al>
2019-02-08 06:41:29 +0100

    Changed typing notifications to a style more consistent with the appop loop
    I let the creation of the typing_notifications label be handled by the scroll_widget, and for updating send it through the appop loop to room.rs, and further through it's children. Also fixed a lot of issues pointed out by Davis

--- fractal-matrix-api/src/backend/types.rs
+++ fractal-matrix-api/src/backend/types.rs
@@ -133,6 +133,7 @@ pub enum BKResponse {
     RoomNotifications(String, i32, i32),
     UserSearch(Vec<Member>),
     Stickers(Vec<StickerGroup>),
+    Typing(HashMap<String, Vec<String>>),
 
     //errors
     UserNameError(Error),
