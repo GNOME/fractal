6c55e6f26df6a2b59cdcbe8c0f108c921780f385
Alejandro Domínguez <adomu@net-c.com>
2019-03-12 12:21:58 +0100

    API, user: Separate endpoint connection from query build

--- fractal-matrix-api/src/r0.rs
+++ fractal-matrix-api/src/r0.rs
@@ -1,5 +1,25 @@
 pub mod account;
+pub mod contact;
 pub mod directory;
 pub mod filter;
+pub mod media;
+pub mod profile;
+pub mod search;
 pub mod sync;
 pub mod thirdparty;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(rename = "lowercase")]
+pub enum Medium {
+    Email,
+    MsIsdn,
+}
+
+#[derive(Clone, Debug, Serialize)]
+pub struct ThreePIDCredentials {
+    pub client_secret: String,
+    pub id_server: String,
+    pub sid: String,
+}
