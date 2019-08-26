c11d1166c5b9594606e36468eea7c6694889173a
Alejandro Domínguez <adomu@net-c.com>
2019-08-26 10:50:22 +0200

    BKResponse, stickers: remove error variants and compose over Result instead

--- fractal-matrix-api/src/backend/stickers.rs
+++ fractal-matrix-api/src/backend/stickers.rs
@@ -42,9 +42,11 @@ pub fn list(bk: &Backend) -> Result<(), Error> {
                 let group = StickerGroup::from_json(sticker_group);
                 stickers.push(group);
             }
-            tx.send(BKResponse::Stickers(stickers)).unwrap();
+            let _ = tx.send(BKResponse::Stickers(Ok(stickers)));
         },
-        |err| tx.send(BKResponse::StickersError(err)).unwrap()
+        |err| {
+            let _ = tx.send(BKResponse::Stickers(Err(err)));
+        }
     );
 
     Ok(())
@@ -75,7 +77,7 @@ pub fn get_sticker_widget_id(bk: &Backend, then: BKCommand) -> Result<(), Error>
             d.lock().unwrap().sticker_widget = widget_id;
 
             if let Some(t) = itx {
-                t.send(then).unwrap();
+                let _ = t.send(then);
             }
         },
         |err| {
@@ -90,7 +92,7 @@ pub fn get_sticker_widget_id(bk: &Backend, then: BKCommand) -> Result<(), Error>
             }
 
             if let Some(t) = itx {
-                t.send(then).unwrap();
+                let _ = t.send(then);
             }
         }
     );
@@ -154,9 +156,11 @@ pub fn purchase(bk: &Backend, group: &StickerGroup) -> Result<(), Error> {
     get!(
         &url,
         |_| if let Some(t) = itx {
-            t.send(BKCommand::ListStickers).unwrap();
+            let _ = t.send(BKCommand::ListStickers);
         },
-        |err| tx.send(BKResponse::StickersError(err)).unwrap()
+        |err| {
+            let _ = tx.send(BKResponse::Stickers(Err(err)));
+        }
     );
 
     Ok(())
