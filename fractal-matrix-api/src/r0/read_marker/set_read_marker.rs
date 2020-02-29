5ac9f550afb4f0b7e170e4443747c9db70120145
Nils Asmussen <nils@os.inf.tu-dresden.de>
2020-02-29 16:36:15 +0100

    Support matrix servers in a subdirectory.
    Previously, the user-provided URL was join'ed with an absolute path
    (e.g. "/_matrix/..."). Thus, the path was replaced with the given one.
    In other words, fractal assumed that matrix servers are always available
    at "/".
    
    This commit appends a relative path, so that matrix servers in
    subdirectories are supported as well.

--- fractal-matrix-api/src/r0/read_marker/set_read_marker.rs
+++ fractal-matrix-api/src/r0/read_marker/set_read_marker.rs
@@ -27,7 +27,7 @@ pub fn request(
 ) -> Result<Request, Error> {
     let url = base
         .join(&format!(
-            "/_matrix/client/r0/rooms/{}/read_markers",
+            "_matrix/client/r0/rooms/{}/read_markers",
             room_id
         ))
         .expect("Malformed URL in set_read_marker");
