7467d9f9afc988c7a93b5ec3f65e987dee33cc46
Alejandro Domínguez <adomu@net-c.com>
2020-07-05 13:40:37 +0200

    Use clone! macro from glib

--- fractal-gtk/src/appop/user.rs
+++ fractal-gtk/src/appop/user.rs
@@ -1,7 +1,7 @@
 use gtk::prelude::*;
 
 use crate::backend::user;
-use crate::clone;
+use glib::clone;
 
 use std::path::PathBuf;
 use std::thread;
@@ -22,7 +22,7 @@ impl AppOp {
     pub fn get_username(&self) {
         let login_data = unwrap_or_unit_return!(self.login_data.clone());
 
-        thread::spawn(clone!(login_data => move || {
+        thread::spawn(clone!(@strong login_data => move || {
             match user::get_username(login_data.server_url, login_data.uid) {
                 Ok(username) => {
                     APPOP!(set_username, (username));
@@ -33,7 +33,7 @@ impl AppOp {
             }
         }));
 
-        thread::spawn(clone!(login_data => move || {
+        thread::spawn(clone!(@strong login_data => move || {
             match user::get_avatar(login_data.server_url, login_data.uid) {
                 Ok(path) => {
                     APPOP!(set_avatar, (path));
